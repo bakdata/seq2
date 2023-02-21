c49998cea7e654b03386e7ce4ae05a84bff72109
Philipp Schirmer <philipp.schirmer@bakdata.com>
2023-02-21 09:14:12 +0100

    Add methods for optionally joining streams on delimiter

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,12 @@ description = "This library is an extension to the great jOOλ library"
 
 
 plugins {
-    id("net.researchgate.release") version "2.8.1"
+    `java-library`
+    id("net.researchgate.release") version "3.0.2"
     id("com.bakdata.sonar") version "1.1.7"
     id("com.bakdata.sonatype") version "1.1.7"
-    id("org.hildan.github.changelog") version "0.8.0"
-    id("io.freefair.lombok") version "5.3.3.3"
+    id("org.hildan.github.changelog") version "1.12.1"
+    id("io.freefair.lombok") version "6.6.1"
 }
 
 allprojects {
@@ -39,22 +40,28 @@ configure<org.hildan.github.changelog.plugin.GitHubChangelogExtension> {
 allprojects {
     apply(plugin = "java-library")
 
-    configure<JavaPluginConvention> {
+    configure<JavaPluginExtension> {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
 
     dependencies {
-        "api"(group = "org.jooq", name = "jool", version = "0.9.14")
+        api(group = "org.jooq", name = "jool", version = "0.9.14")
 
-        val junitVersion = "5.7.2"
-        "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-        "testImplementation"("org.junit.jupiter:junit-jupiter-params:$junitVersion")
-        "testImplementation"("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-        "testImplementation"(group = "org.assertj", name = "assertj-core", version = "3.20.2")
+        val junitVersion = "5.9.2"
+        testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
+        testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
+        testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-params", version = junitVersion)
+        testImplementation(group = "org.assertj", name = "assertj-core", version = "3.24.2")
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+release {
+    git {
+        requireBranch.set("master")
+    }
 }
--- src/main/java/com/bakdata/util/seq2/BaseSeq.java
+++ src/main/java/com/bakdata/util/seq2/BaseSeq.java
@@ -1,14 +1,25 @@
 /*
- * Copyright (c), 2019 bakdata GmbH
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * MIT License
+ *
+ * Copyright (c) 2023 bakdata
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package com.bakdata.util.seq2;
@@ -632,10 +643,40 @@ public interface BaseSeq<T> {
     }
 
     /**
+     * Consume a stream and concatenate all elements using a separator or return an empty Optional if the stream is
+     * empty.
+     *
+     * @param delimiter the delimiter to be used between each element prefix – the sequence of characters to be used at
+     * the
+     * @return concatenated elements or empty Optional
+     * @see #toString(String)
+     */
+    default Optional<String> toStringOrEmpty(final String delimiter) {
+        final String seq = this.toString(delimiter);
+        return seq.isEmpty() ? Optional.empty() : Optional.of(seq);
+    }
+
+    /**
      * @see Seq#toString(CharSequence, CharSequence, CharSequence)
      */
     default String toString(final String delimiter, final String prefix, final String suffix) {
         return this.toSeq().toString(delimiter, prefix, suffix);
+    }
+
+    /**
+     * Consume a stream and concatenate all elements using a separator or return an empty Optional if the stream is
+     * empty. Additionally, the string is prefixed and suffixed.
+     *
+     * @param delimiter the delimiter to be used between each element prefix – the sequence of characters to be used at
+     * the
+     * @param prefix the sequence of characters to be used at the beginning of the joined result
+     * @param suffix the sequence of characters to be used at the end of the joined result
+     * @return concatenated elements or empty Optional
+     * @see #toString(String)
+     */
+    default Optional<String> toStringOrEmpty(final String delimiter, final String prefix, final String suffix) {
+        final String seq = this.toString(delimiter, prefix, suffix);
+        return seq.equals(prefix + suffix) ? Optional.empty() : Optional.of(seq);
     }
 
     /**
