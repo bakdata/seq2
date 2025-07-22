4974c11d2b3ad8f815792ab5100c5558d44fabd7
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-07-22 15:12:02 +0200

    Upgrade Gradle to 8.14 (#19)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,26 +3,23 @@ description = "This library is an extension to the great jOOλ library"
 
 plugins {
     `java-library`
-    id("net.researchgate.release") version "3.0.2"
-    id("com.bakdata.sonar") version "1.1.17"
-    id("com.bakdata.sonatype") version "1.2.1"
-    id("org.hildan.github.changelog") version "2.2.0"
-    id("io.freefair.lombok") version "8.4"
+    id("com.bakdata.release") version "1.10.0"
+    id("com.bakdata.sonar") version "1.10.0"
+    id("com.bakdata.sonatype") version "1.10.0"
+    id("io.freefair.lombok") version "8.14"
 }
 
-allprojects {
-    group = "com.bakdata.${rootProject.name}"
+group = "com.bakdata.${rootProject.name}"
 
-    tasks.withType<Test> {
-        maxParallelForks = 4
-    }
-
-    repositories {
-        mavenCentral()
-    }
+tasks.withType<Test> {
+    maxParallelForks = 4
 }
 
-configure<com.bakdata.gradle.SonatypeSettings> {
+repositories {
+    mavenCentral()
+}
+
+publication {
     developers {
         developer {
             name.set("Philipp Schirmer")
@@ -31,38 +28,17 @@ configure<com.bakdata.gradle.SonatypeSettings> {
     }
 }
 
-configure<org.hildan.github.changelog.plugin.GitHubChangelogExtension> {
-    githubUser = "bakdata"
-    futureVersionTag = findProperty("changelog.releaseVersion")?.toString()
-    sinceTag = findProperty("changelog.sinceTag")?.toString()
-}
-
-allprojects {
-    apply(plugin = "java-library")
-
-    configure<JavaPluginExtension> {
-        toolchain {
-            languageVersion = JavaLanguageVersion.of(11)
-        }
-    }
-
-    dependencies {
-        api(group = "org.jooq", name = "jool", version = "0.9.14")
-
-        val junitVersion = "5.9.2"
-        testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
-        testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
-        testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-params", version = junitVersion)
-        testImplementation(group = "org.assertj", name = "assertj-core", version = "3.24.2")
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(11)
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
+dependencies {
+    api(group = "org.jooq", name = "jool", version = "0.9.15")
 
-release {
-    git {
-        requireBranch.set("master")
-    }
+    val junitVersion = "5.13.3"
+    testRuntimeOnly(group = "org.junit.platform", name = "junit-platform-launcher")
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = junitVersion)
+    testImplementation(group = "org.assertj", name = "assertj-core", version = "3.27.3")
 }
